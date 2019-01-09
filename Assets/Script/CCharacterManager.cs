ff6cf6aaca37aa48660a5d6065f06075e7d65e57
hanul.moon <skdsodkd@gmail.com>
2019-01-09 20:42:38 +0900

    UI추가

--- Assets/Script/CCharacterManager.cs
+++ Assets/Script/CCharacterManager.cs
@@ -1,24 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class CData
 {
     public int cnt;
     public string[] orders;
     public string[] names;
-
+   
     public CData()
     {
         cnt = 6;
 
         orders = new string[6];
-        orders[0] = "civil";
-        orders[1] = "civil";
+        orders[0] = "civil2";
+        orders[1] = "civil1";
         orders[2] = "policer";
         orders[3] = "doctor";
         orders[4] = "mapia";
-        orders[5] = "civil";
+        orders[5] = "civil3";
         names = new string[6];
         names[0] = "taeeun";
         names[1] = "hanul";
@@ -34,22 +34,24 @@ public class CData
 public class CCharacterManager : MonoBehaviour {
     [SerializeField]
     private GameObject[] characters;
-
+    public GameObject[] abc;
+    public Text[] text;
     CData data;
-
+  
     Vector2[] vectors;
     // Use this for initialization
     
 
     void Start()
     {
+        
         data = new CData();
 
         vectors = new Vector2[6];
 
         for(int i = 0; i < 6; ++i)
         {
-            vectors[i] = new Vector2(i, i -1);
+            vectors[i] = abc[i].gameObject.transform.position;
         }
 
         Init(data);
@@ -67,20 +69,31 @@ public class CCharacterManager : MonoBehaviour {
         {
             switch (data.orders[i])
             {
-                case "mapia":
+                case "civil1":
+                    type = 0;
+                    break;
+                case "civil2":
                     type = 1;
                     break;
-                case "policer":
+                case "civil3":
                     type = 2;
                     break;
                 case "doctor":
                     type = 3;
                     break;
-                default:
-                    type = 0;
+                case "policer":
+                    type = 4;
                     break;
+                default:
+                    type = 5;
+                    break;
+
             }
             GameObject clone = Instantiate(characters[type], vectors[i], Quaternion.identity) as GameObject;
+            clone.transform.parent = abc[i].transform;
+            clone.transform.localScale = abc[i].transform.localScale * 0.92f;
+            text[i].text = data.names[i];
+           
             clone.name = data.names[i];
         }
     }
